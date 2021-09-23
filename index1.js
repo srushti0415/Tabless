329f7de7a4a914589334645556493e7e45a4963e
srushti0415 <srushtikulkarni15@gmail.com>
2021-09-23 22:34:03 +0530

    Squre roots

---
+++ index1.js
@@ -0,0 +1,20 @@
+
+// let a = 2;
+// let b = 2;
+// while ( a&&b <10 ) {
+//     a*b;
+//     // console.log(a);
+//     a++;
+//     b++;
+// console.log(a,b);
+
+// }
+
+let a = 10;
+let b = 10;
+let c = a*b;
+while (a&&b<=10) {
+   a&&b++;
+    console.log(c);
+}
+// console.log()
