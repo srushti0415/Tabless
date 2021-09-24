322126fcc25f990d835a02d6262e74852755f40d
srushti0415 <srushtikulkarni15@gmail.com>
2021-09-24 14:58:45 +0530

    updated SQ

--- index1.js
+++ index1.js
@@ -10,11 +10,10 @@
 
 // }
 
-let a = 10;
-let b = 10;
-let c = a*b;
-while (a&&b<=10) {
-   a&&b++;
-    console.log(c);
+let a = 20;
+while (a<=20) {
+   a=a++;
+   a=a*a;
+    console.log(a);
 }
-// console.log()
+
