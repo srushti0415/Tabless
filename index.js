cfc6621218d6c8d30a3137cd27c8a1b1ac1564aa
srushti0415 <srushtikulkarni15@gmail.com>
2021-10-09 13:19:12 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-// console.log("Tables")
-// for (let i = 2; i <= 20; i += 2) { console.log(i) }
-
-
-let s = 17;
-    while (s <= 170) {
-        console.log(s);
-        s += 17;
+function EvenorOdd(a){
+    if(a%2 == 0){
+        console.log("this is even no")
     }
+    else {
+        console.log("odd no.")
+    }
+}
+EvenorOdd(6);
 
-
+let b = 0;
+function Tables(a) {
+    while (b < 10) {
+        b++;
+        console.log(a * b)
+    }
+}
+Tables(19);
