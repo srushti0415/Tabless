d0590ee66bbfcd03490e784024cae3ace9c6822d
srushti0415 <srushtikulkarni15@gmail.com>
2021-11-09 22:20:16 +0530

    updated

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-function EvenorOdd(a){
-    if(a%2 == 0){
-        console.log("this is even no")
-    }
-    else {
-        console.log("odd no.")
-    }
-}
-EvenorOdd(6);
+// function EvenorOdd(a){
+//     if(a%2 == 0){
+//         console.log("this is even no")
+//     }
+//     else {
+//         console.log("odd no.")
+//     }
+// }
+// EvenorOdd(6);
 
-let b = 0;
-function Tables(a) {
-    while (b < 10) {
-        b++;
-        console.log(a * b)
+// let b = 1;
+// function Tables(a) {
+//     while (b <= 10) {
+//         console.log(a * b)
+//         b++;
+//     }
+// }
+// Tables(15);
+
+function squareroot(a,b){
+    while (a==b){
+        console.log(a*b)
+        a++;
     }
 }
-Tables(19);
+squareroot(8,8)
